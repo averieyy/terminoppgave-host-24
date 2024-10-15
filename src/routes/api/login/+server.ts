21cc2b025f42fd98b6369df91b286b448569ee10
averieyy <58344917+averieyy@users.noreply.github.com>
2024-10-15 16:40:02 +0200

    added login page

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,6 +1,7 @@
 import { User } from "$lib/server/user";
 import type { RequestHandler } from "@sveltejs/kit";
 
+// Debug purposes
 export const GET: RequestHandler = ({ cookies }) => {
   const user = new User('user');
   const cookie = user.genCookie();
@@ -8,4 +9,23 @@ export const GET: RequestHandler = ({ cookies }) => {
   cookies.set('token', cookie, { path: '/', secure: false });
 
   return new Response('asdf');
+}
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+  const { username, password } : { username: string, password: string } = await request.json();
+
+  if (!username || !password) {
+    return new Response('Username or password missing', { status: 400 });
+  }
+
+  if (password.length < 8) {
+    return new Response('Password has to be longer than 8 characters', { status: 400 });
+  }
+
+  const user = new User(username);
+  const cookie = user.genCookie();
+
+  cookies.set('token', cookie, { path: '/', secure: false });
+
+  return new Response('Logged in as ' + username);
 }
---
+++ src/routes/app/login/+page.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+
+  let redirectLocation = $page.url.searchParams.get('redirect');
+
+  let username: string;
+  let password: string;
+
+  let detailError = false;
+  let errorMessage = '';
+
+  function logIn() {
+    fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }).then(async resp => {
+      if (resp.ok) {
+        goto(redirectLocation || '/app/channel/12');
+      }
+      else {
+        username = '';
+        password = '';
+        detailError = true;
+        errorMessage = await resp.text() || 'An error occured when trying to log in';
+      }
+    });
+  }
+</script>
+
+<main>
+  <form class="loginform" on:submit={logIn}>
+    <h2>Log in</h2>
+    {#if errorMessage !== ''}
+      <span class="errormessage">{errorMessage}</span>
+    {/if}
+    <input class={`${detailError && 'error'} inputform`} placeholder="Username" type="text" bind:value={username} />
+    <input placeholder="Password (not in use)" type="password" bind:value={password} />
+    <button on:click={logIn}>Log in</button>
+  </form>
+</main>
+
+<style>
+  h2 {
+    all: unset;
+    border-bottom: 2px solid var(--fg1);
+  }
+  main {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    flex-direction: column;
+  }
+  .loginform {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+    padding: 1rem;
+    background-color: var(--bg3);
+  }
+  input, button {
+    background-color: var(--bg4);
+    border: none;
+    color: var(--fg1);
+    padding: .5rem;
+  }
+  .errormessage {
+    color: var(--red);
+    font-size: 90%;
+    font-style: italic;
+  }
+</style>
