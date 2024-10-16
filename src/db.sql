d9e4c9168ea299910b7745d70fb6d63ac0b5e8f6
averieyy <58344917+averieyy@users.noreply.github.com>
2024-10-16 11:00:31 +0200

    created a basic db.sql file

---
+++ src/db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL CONSTRAINT unique_username UNIQUE
+);
+
+CREATE TABLE channel (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  content TEXT NOT NULL,
+  senderid INT NOT NULL REFERENCES users(id),
+  channelid INT NOT NULL REFERENCES channel(id),
+  sentat TIMESTAMP NOT NULL
+);
